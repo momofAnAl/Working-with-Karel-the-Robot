05e99f393766ef33fa5b21ff14cb1ae666ebd7d0
Anh Tran <anhtr077@gmail.com>
2023-04-26 22:36:27 -0700

    Add build_hospital.py

---
+++ build_hospital.py
@@ -0,0 +1,48 @@
+from karel.stanfordkarel import *
+
+# Here is a place to program your Section problem
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    while front_is_clear():
+        if beepers_present():
+            build_hospital()
+            
+        safe_move()
+        
+def safe_move():
+    if front_is_clear():
+        move()
+        
+#building hospital 
+def build_hospital():
+    ascend_hurdle()
+    move()
+    descend_hurdle()
+    
+def ascend_hurdle():
+    turn_left()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_right()
+    
+def descend_hurdle():
+    turn_right()
+    #put_beeper()
+    for i in range(2):
+        put_beeper()
+        move()
+    put_beeper()
+    turn_left()
+    
+def turn_right():
+    for i in range(3):
+        turn_left()
+if __name__ == '__main__':
+    main()
